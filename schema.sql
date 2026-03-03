4809b4a2d90c9c20f27524aa72db37439c76a8ba
Állephe_Lykoos <allephe.rocha@gmail.com>
2026-03-03 07:30:21 -0300

    Atualizar o schema.sql

--- schema.sql
+++ schema.sql
@@ -2,6 +2,10 @@ CREATE TABLE films (
     id SERIAL PRIMARY KEY,
     title TEXT,
     release_year INTEGER,
+    country TEXT,
+    duration TEXT,
     language TEXT,
-    certification TEXT
+    certification TEXT,
+    gross TEXT,
+    budget TEXT
 );
